962612502212e04bfd1e8abf4b2bd615b71feed3
ReevesAk <reeves.akwa@gmail.com>
2021-05-15 22:42:18 +0100

    corrected the creation of nameless accounts

--- tinywallet/tinywallet/screens.py
+++ tinywallet/tinywallet/screens.py
@@ -2022,6 +2022,7 @@ class NewAccountScreen(Screen):
         name = self.nameField.text()
         if not name:
             app.appWindow.showError("must provide name")
+            return
 
         def doneCreateAcct(acct):
             if not acct:
